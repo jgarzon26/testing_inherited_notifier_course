3899e58a0f549870adaf37c57bbf27822f9b6c20
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-04-11 11:58:02 +0800

    Init home

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -5,10 +5,41 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const defaultSize = 200.00;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
+      body: Column(
+        children: [
+          Slider(
+            value: 0.0,
+            onChanged: (value) {},
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                height: defaultSize,
+                color: Colors.yellow,
+              ),
+              Container(
+                height: defaultSize,
+                color: Colors.blue,
+              ),
+            ].expandEqually().toList(),
+          ),
+        ],
+      ),
     );
   }
+}
+
+extension ExpandEqually on Iterable<Widget> {
+  Iterable<Widget> expandEqually() => map(
+        (e) => Expanded(
+          child: e,
+        ),
+      );
 }
